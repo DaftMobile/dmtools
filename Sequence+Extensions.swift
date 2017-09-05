8c1b857e6092921814afce3b5bda72cf2786fe49
Michal Dabrowski <michal.dabrowski@daftcode.pl>
2017-09-05 15:30:51 +0200

    Add sequence group

---
+++ Sequence+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Sequence+Extensions.swift
+//  Pods
+//
+//  Created by Kajetan Dąbrowski on 05/09/2017.
+//
+//
+
+import Foundation
+
+public extension Sequence {
+	func group<U: Hashable>(by key: (Iterator.Element) -> U) -> [U: [Iterator.Element]] {
+		var categories: [U: [Iterator.Element]] = [:]
+		for element in self {
+			let key = key(element)
+			if case nil = categories[key]?.append(element) {
+				categories[key] = [element]
+			}
+		}
+		return categories
+	}
+}
