71d2bbb3714f3f368ac422153832969ce2528a4b
Michal Dabrowski <michal.dabrowski@daftcode.pl>
2017-10-06 13:22:50 +0200

    Add Low Pass filter

--- DMTools.podspec
+++ DMTools.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'DMTools'
-  s.version          = '0.3.10'
+  s.version          = '0.3.11'
   s.summary          = 'Shared Tools for DaftMobile coding.'
 
 
---
+++ DMTools/Classes/LowPassValue.swift
@@ -0,0 +1,39 @@
+//
+//  LowPassValue.swift
+//  DMTools
+//
+//  Created by Kajetan Dąbrowski on 06/10/2017.
+//
+
+import Foundation
+
+public struct LowPassFilterSignal<T: FloatingPoint> {
+	/// Current signal value
+	public private(set) var value: T
+
+	/// A scaling factor in the range 0.0..<1.0 that determines
+	/// how resistant the value is to change
+	public let filterFactor: T
+
+	/// Update the value, using filterFactor to attenuate changes
+	public mutating func update(newValue: T) {
+		value = filterFactor * value + (T(1) - filterFactor) * newValue
+	}
+
+	public init(value: T, filterFactor: T) {
+		self.value = value
+		self.filterFactor = filterFactor
+	}
+}
+
+extension LowPassFilterSignal: CustomDebugStringConvertible {
+	public var debugDescription: String {
+		return (value as? CustomDebugStringConvertible)?.debugDescription ?? "Low pass value"
+	}
+}
+
+extension LowPassFilterSignal: Equatable {
+	public static func == (lhs: LowPassFilterSignal, rhs: LowPassFilterSignal) -> Bool {
+		return lhs.value == rhs.value && lhs.filterFactor == rhs.filterFactor
+	}
+}
