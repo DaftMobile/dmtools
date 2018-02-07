b5bed862bbfee6cb1b7b5d91a419bd1dad0593a7
Bartosz Kamiński <bartosz.kaminski@daftcode.pl>
2018-02-07 11:32:57 +0100

    Add UILayoutPriority extension with + and - functions

---
+++ UILayoutPriority+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  UILayoutPriority+Extensions.swift
+//  DMTools
+//
+//  Created by Bartosz Kamiński on 07/02/2018.
+//
+
+import UIKit
+
+public extension UILayoutPriority {
+
+	/// Adds Float to UILayoutPriority raw value
+	public static func + (lhs: UILayoutPriority, rhs: Float) -> UILayoutPriority {
+		return UILayoutPriority(lhs.rawValue + rhs)
+	}
+
+	/// Subtracts Float from UILayoutPriority raw value
+	public static func - (lhs: UILayoutPriority, rhs: Float) -> UILayoutPriority {
+		return UILayoutPriority(lhs.rawValue - rhs)
+	}
+}
