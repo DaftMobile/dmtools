6da7d28183732c0ae3278ffa96d2217f0aac0d74
Michał Dąbrowski <michal.dabrowski@daftcode.pl>
2020-09-18 19:48:05 +0200

    Add package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "DMTools",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(name: "DMTools", targets: ["DMTools"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .branch("v9.0.0-rc.3"))
+    ],
+    targets: [
+        .target(name: "DMTools", path: "DMTools"),
+        .testTarget(name: "Tests", dependencies: [.target(name: "DMTools"), .byName(name: "Quick"), .byName(name: "Nimble")], path: "Tests"),
+    ]    
+)
