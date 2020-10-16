76f52305e507c7e0f49528f7c399f90c48ed5fd8
Taras Holets <taras.holets@daftmobile.com>
2020-10-16 09:15:31 +0200

    Add round specific corners method to UIView

--- DMTools/Classes/Extensions/UIView/UIView+Extensions.swift
+++ DMTools/Classes/Extensions/UIView/UIView+Extensions.swift
@@ -48,6 +48,19 @@ public extension UIView {
 	}
 
 	/**
+	Rounds specific corners of `UIView`
+
+	- parameter corners: collection of corners to round
+	- parameter radius: corner radius
+	*/
+	func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+		let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+		let mask = CAShapeLayer()
+		mask.path = path.cgPath
+		layer.mask = mask
+	}
+
+	/**
 	Adds shake animation to the receiver
 	
 	- parameter offset:		shake animation max offset. default value 15.0
