6e08cc0a6712a33fba70b0489bc69c234e944082
Taras Holets <taras.holets@daftmobile.com>
2020-10-16 08:55:59 +0200

    Add extension to UIImageView and method for image frame counting

---
+++ DMTools/Classes/Extensions/UIImageView/UIImageView+Extensions.swift
@@ -0,0 +1,11 @@
+import AVFoundation
+
+public extension UIImageView {
+
+	/**
+	- returns: Frame of `UIImage` inside `UIImageView`
+	*/
+	func imageFrame() -> CGRect {
+		return AVMakeRect(aspectRatio: image?.size ?? .zero, insideRect: frame)
+	}
+}
