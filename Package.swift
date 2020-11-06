5567cf01271f925a62dc9730e4b5b2e099d92891
Michał Dąbrowski <michal.dabrowski@daftcode.pl>
2020-11-06 18:25:44 +0100

    Update packages

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -12,10 +12,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .branch("v9.0.0-rc.3"))
+        .package(url: "https://github.com/Quick/Nimble.git", .branch("v9.0.0"))
     ],
     targets: [
         .target(name: "DMTools", path: "DMTools"),
-        .testTarget(name: "Tests", dependencies: [.target(name: "DMTools"), .byName(name: "Quick"), .byName(name: "Nimble")], path: "Tests"),
+        .testTarget(name: "DMToolsTests", dependencies: [.target(name: "DMTools"), .byName(name: "Quick"), .byName(name: "Nimble")]),
     ]    
 )
